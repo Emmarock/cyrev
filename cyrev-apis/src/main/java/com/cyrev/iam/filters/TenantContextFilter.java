36244cf03a148292b43e76781f9427d99d8ebd8f
apata <babajide@nestcoin.com>
2026-03-14 22:23:26 +0000

    updated work on connecting to entra, getting users list from entra, added api for inviting the user, accepting user invite, untested implementation of signup and login with microsoft auth

--- cyrev-apis/src/main/java/com/cyrev/iam/filters/TenantContextFilter.java
+++ cyrev-apis/src/main/java/com/cyrev/iam/filters/TenantContextFilter.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -25,6 +26,8 @@ import java.util.UUID;
 public class TenantContextFilter extends OncePerRequestFilter {
 
     private final SaasTenantRepository saasTenantRepository;
+    private static final AntPathMatcher matcher = new AntPathMatcher();
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -63,7 +66,11 @@ public class TenantContextFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
         String path = request.getRequestURI();
-        List<String> tenantContextExcludedPaths = List.of("/api/auth/");
-        return tenantContextExcludedPaths.contains(path);
+
+        List<String> patterns = List.of(
+                "/api/auth/**"
+        );
+
+        return patterns.stream().anyMatch(p -> matcher.match(p, path));
     }
 }
