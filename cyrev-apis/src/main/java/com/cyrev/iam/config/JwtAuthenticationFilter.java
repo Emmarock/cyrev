5bc824f7a7280aeddc5974e837e8069e1298ee97
apata <babajide@nestcoin.com>
2026-03-28 19:14:19 +0000

    fixed issue with login without tenant details

--- cyrev-apis/src/main/java/com/cyrev/iam/config/JwtAuthenticationFilter.java
+++ cyrev-apis/src/main/java/com/cyrev/iam/config/JwtAuthenticationFilter.java
@@ -43,14 +43,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         UUID userId = UUID.fromString(claims.getSubject());
         String username = claims.get("username", String.class);
-        String orgId = claims.get("orgId", String.class);
+        String tenantId = claims.get("tenantId", String.class);
 
         List<String> roles = claims.get("roles", String.class).lines().toList();
         List<GrantedAuthority> authorities = roles.stream()
                 .map(r -> new SimpleGrantedAuthority("ROLE_" + r))
                 .collect(Collectors.toList());
 
-        AuthenticatedUser principal = new AuthenticatedUser(userId, username, orgId, authorities);
+        AuthenticatedUser principal = new AuthenticatedUser(userId, username, tenantId, authorities);
 
         UsernamePasswordAuthenticationToken authentication =
                 new UsernamePasswordAuthenticationToken(principal, null, authorities);
