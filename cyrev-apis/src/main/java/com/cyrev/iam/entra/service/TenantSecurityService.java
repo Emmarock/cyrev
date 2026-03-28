daf5f4690131329edff532b4d8332166132c3b35
apata <babajide@nestcoin.com>
2026-03-28 18:45:25 +0000

    fixed issue with login without tenant details

--- cyrev-apis/src/main/java/com/cyrev/iam/entra/service/TenantSecurityService.java
+++ cyrev-apis/src/main/java/com/cyrev/iam/entra/service/TenantSecurityService.java
@@ -20,7 +20,7 @@ public class TenantSecurityService {
     private final UserService userService;
     public void validateTenantAdmin(AuthenticatedUser user, TenantContext tenantContext) {
 
-        String tenantId = tenantContext.getEntraTenantId();
+        UUID tenantId = tenantContext.getInternalTenantId();
 
         boolean isAdmin = checkIfUserIsTenantAdmin(user.getUserId(), tenantId);
 
@@ -29,8 +29,8 @@ public class TenantSecurityService {
         }
     }
 
-    private boolean checkIfUserIsTenantAdmin(UUID userId, String tenantId) {
-        User user = userService.findTenantUser(userId, UUID.fromString(tenantId));
+    private boolean checkIfUserIsTenantAdmin(UUID userId, UUID tenantId) {
+        User user = userService.findTenantUser(userId, tenantId);
         return Role.ADMIN.equals(user.getRole()) || Role.SUPER_ADMIN.equals(user.getRole());
     }
 }
