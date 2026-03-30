7e0eca686e550954cfe6912e0e65502dabf989a4
apata <babajide@nestcoin.com>
2026-03-30 12:51:57 +0100

    fix issue of registering users without tenants

--- cyrev-apis/src/main/java/com/cyrev/iam/annotations/TenantAdminAspect.java
+++ cyrev-apis/src/main/java/com/cyrev/iam/annotations/TenantAdminAspect.java
@@ -5,6 +5,7 @@ import com.cyrev.common.entities.TenantContextHolder;
 import com.cyrev.iam.domain.AuthenticatedUser;
 import com.cyrev.iam.entra.service.TenantSecurityService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.security.access.AccessDeniedException;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class TenantAdminAspect {
 
     private final TenantSecurityService tenantSecurityService;
@@ -27,10 +29,12 @@ public class TenantAdminAspect {
                 .getAuthentication();
 
         if (authentication == null || !authentication.isAuthenticated()) {
+            log.error("User is not authenticated");
             throw new AccessDeniedException("User is not authenticated");
         }
 
         if (!(authentication.getPrincipal() instanceof AuthenticatedUser user)) {
+            log.error("Invalid authenticated principal {}", authentication.getPrincipal());
             throw new AccessDeniedException("Invalid authenticated principal");
         }
 
