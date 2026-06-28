307dc8ec87c3f6e50a1c7013613dee1408e513e5
apata <babajide@nestcoin.com>
2026-06-28 21:48:55 +0100

    added functionality to provisionExchangeAdminRole

--- cyrev-apis/src/main/java/com/cyrev/iam/entra/service/onboarding/EntraConsentService.java
+++ cyrev-apis/src/main/java/com/cyrev/iam/entra/service/onboarding/EntraConsentService.java
@@ -31,21 +31,9 @@ public class EntraConsentService {
             String stateJson = objectMapper.writeValueAsString(payload);
             String stateEncoded = Base64.getUrlEncoder()
                     .encodeToString(stateJson.getBytes(StandardCharsets.UTF_8));
-            String scope = String.join(" ",
-                    "https://graph.microsoft.com/User.ReadWrite.All",
-                    "https://graph.microsoft.com/Exchange.ManageAsApp",
-                    "https://graph.microsoft.com/Group.ReadWrite.All",
-                    "https://graph.microsoft.com/AppRoleAssignment.ReadWrite.All",
-                    "https://graph.microsoft.com/Application.Read.All",
-                    "https://graph.microsoft.com/Organization.Read.All",
-                    "https://graph.microsoft.com/EntitlementManagement.Read.All",
-                    "https://graph.microsoft.com/RoleManagement.ReadWrite.All",
-                    "https://graph.microsoft.com/RoleManagement.ReadWrite.Directory"
-            );
             return props.getAuthority()
-                    + "/organizations/v2.0/adminconsent"
+                    + "/common/adminconsent"
                     + "?client_id=" + URLEncoder.encode(props.getAppId(), StandardCharsets.UTF_8)
-                    + "&scope=" + URLEncoder.encode(scope, StandardCharsets.UTF_8)
                     + "&redirect_uri=" + URLEncoder.encode(props.getConsentRedirectUri(), StandardCharsets.UTF_8)
                     + "&state=" + URLEncoder.encode(stateEncoded, StandardCharsets.UTF_8);
         } catch (JsonProcessingException e) {
